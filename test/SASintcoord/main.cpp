ac02623744473b9e3ba43d325a80a09323f012a3
YifanShenSZ <yshen57@jhu.edu>
2021-10-15 22:10:02 -0400

    new test case for SASIC

--- test/SASintcoord/main.cpp
+++ test/SASintcoord/main.cpp
@@ -1,47 +1,131 @@
+#include <unordered_map>
+
 #include <CppLibrary/chemistry.hpp>
 
 #include <tchem/intcoord.hpp>
 
-int main() {
-    std::cout << "This is a test program on Torch-Chemistry module 'SASintcoord'\n"
-              << "Correct routines should print close to 0\n";
+c10::TensorOptions top = at::TensorOptions().dtype(torch::kFloat64);
 
-    c10::TensorOptions top = at::TensorOptions().dtype(torch::kFloat64);
-
+void test_sasic() {
     tchem::IC::SASICSet set("whatever", "whatever", "SAS.in");
 
-    CL::chem::xyz<double> geom_c2v("sad-A1.xyz", true);
-    std::vector<double> coords_c2v = geom_c2v.coords();
-    at::Tensor r_c2v = at::from_blob(coords_c2v.data(), coords_c2v.size(), top);
-    at::Tensor q_c2v = set.IntCoordSet::operator()(r_c2v);
-    std::vector<at::Tensor> SASgeom_c2v = set(q_c2v);
-    std::cout << "\nC2v: "
-              << SASgeom_c2v[1].norm().item<double>() << ' '
-              << SASgeom_c2v[2].norm().item<double>() << ' '
-              << SASgeom_c2v[3].norm().item<double>() << '\n';
+    std::vector<std::string> prefixes({"E", "N", "B", "I", "NB", "NI", "BI", "NBI"});
+    std::vector<std::vector<at::Tensor>> SASgeoms(8);
+    for (size_t i = 0; i < 8; i++) {
+        std::string file = prefixes[i] + ".xyz";
+        CL::chem::xyz<double> geom(file, true);
+        std::vector<double> coords = geom.coords();
+        at::Tensor r = at::from_blob(coords.data(), coords.size(), top);
+        at::Tensor q = set.IntCoordSet::operator()(r);
+        SASgeoms[i] = set(q);
+    }
 
-    CL::chem::xyz<double> geom_csoop("min-A1.xyz", true);
-    std::vector<double> coords_csoop = geom_csoop.coords();
-    at::Tensor r_csoop = at::from_blob(coords_csoop.data(), coords_csoop.size(), top);
-    at::Tensor q_csoop = set.IntCoordSet::operator()(r_csoop);
-    std::vector<at::Tensor> SASgeom_csoop = set(q_csoop);
-    std::cout << "\nCs out of plane: "
-              << SASgeom_csoop[2].norm().item<double>() << ' '
-              << SASgeom_csoop[3].norm().item<double>() << '\n';
+    double difference = 0.0;
+    std::unordered_map<std::string, size_t> prefix2index{
+        {"E", 0}, {"N", 1}, {"B", 2}, {"I", 3},
+        {"NB", 4}, {"NI", 5}, {"BI", 6}, {"NBI", 7}
+    };
+    std::cout << "\nSymmetry adapted and scaled internal coordinate:\n";
+    // irreducible 1: totally symmetric
+    for (const std::string & prefix : {"N", "B", "I", "NB", "NI", "BI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][0] - SASgeoms[0][0]).norm().item<double>();
+    }
+    std::cout << "irreducible 1: " << difference << '\n';
+    // irreducible 2: asymmetric N
+    difference = 0.0;
+    difference += (SASgeoms[0][1] + SASgeoms[1][1]).norm().item<double>();
+    for (const std::string & prefix : {"B", "I", "BI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][1] - SASgeoms[0][1]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"NB", "NI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][1] - SASgeoms[1][1]).norm().item<double>();
+    }
+    std::cout << "irreducible 2: " << difference << '\n';
+    // irreducible 3: asymmetric B
+    difference = 0.0;
+    difference += (SASgeoms[0][2] + SASgeoms[2][2]).norm().item<double>();
+    for (const std::string & prefix : {"N", "I", "NI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][2] - SASgeoms[0][2]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"NB", "BI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][2] - SASgeoms[2][2]).norm().item<double>();
+    }
+    std::cout << "irreducible 3: " << difference << '\n';
+    // irreducible 4: asymmetric I
+    difference = 0.0;
+    difference += (SASgeoms[0][3] + SASgeoms[3][3]).norm().item<double>();
+    for (const std::string & prefix : {"N", "B", "NB"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][3] - SASgeoms[0][3]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"NI", "BI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][3] - SASgeoms[3][3]).norm().item<double>();
+    }
+    std::cout << "irreducible 4: " << difference << '\n';
+    // irreducible 5: asymmetric N & B
+    difference = 0.0;
+    difference += (SASgeoms[0][4] + SASgeoms[1][4]).norm().item<double>();
+    for (const std::string & prefix : {"I", "NB", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][4] - SASgeoms[0][4]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"B", "NI", "BI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][4] - SASgeoms[1][4]).norm().item<double>();
+    }
+    std::cout << "irreducible 5: " << difference << '\n';
+    // irreducible 6: asymmetric N & I
+    difference = 0.0;
+    difference += (SASgeoms[0][5] + SASgeoms[1][5]).norm().item<double>();
+    for (const std::string & prefix : {"B", "NI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][5] - SASgeoms[0][5]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"I", "NB", "BI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][5] - SASgeoms[1][5]).norm().item<double>();
+    }
+    std::cout << "irreducible 6: " << difference << '\n';
+    // irreducible 7: asymmetric B & I
+    difference = 0.0;
+    difference += (SASgeoms[0][6] + SASgeoms[2][6]).norm().item<double>();
+    for (const std::string & prefix : {"N", "BI", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][6] - SASgeoms[0][6]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"I", "NB", "NI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][6] - SASgeoms[2][6]).norm().item<double>();
+    }
+    std::cout << "irreducible 7: " << difference << '\n';
+    // irreducible 8: asymmetric N & B & I
+    difference = 0.0;
+    difference += (SASgeoms[0][7] + SASgeoms[1][7]).norm().item<double>();
+    for (const std::string & prefix : {"NB", "NI", "BI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][7] - SASgeoms[0][7]).norm().item<double>();
+    }
+    for (const std::string & prefix : {"B", "I", "NBI"}) {
+        size_t index = prefix2index[prefix];
+        difference += (SASgeoms[index][7] - SASgeoms[1][7]).norm().item<double>();
+    }
+    std::cout << "irreducible 8: " << difference << '\n';
+}
 
-    CL::chem::xyz<double> geom_csplanar("sad-B1.xyz", true);
-    std::vector<double> coords_csplanar = geom_csplanar.coords();
-    at::Tensor r_csplanar = at::from_blob(coords_csplanar.data(), coords_csplanar.size(), top);
-    at::Tensor q_csplanar = set.IntCoordSet::operator()(r_csplanar);
-    std::vector<at::Tensor> SASgeom_csplanar = set(q_csplanar);
-    std::cout << "\nCs planar: "
-              << SASgeom_csplanar[1].norm().item<double>() << ' '
-              << SASgeom_csplanar[3].norm().item<double>() << '\n';
+void test_Jacobian() {
+    tchem::IC::SASICSet set("whatever", "whatever", "SAS.in");
 
-    CL::chem::xyz<double> geom("slow-1.5.xyz", true);
+    CL::chem::xyz<double> geom("E.xyz", true);
     std::vector<double> coords = geom.coords();
     at::Tensor r = at::from_blob(coords.data(), coords.size(), top);
     at::Tensor q = set.IntCoordSet::operator()(r);
+
     // Backward propagation
     q.set_requires_grad(true);
     std::vector<at::Tensor> SASq = set(q);
@@ -73,7 +157,16 @@ int main() {
             for (size_t row = 0; row < SASJ_N[irred].size(0); row++) SASJ_N[irred][row][column] = subvec[row];
         }
     }
+
     double difference = 0.0;
     for (size_t irred = 0; irred < SASq.size(); irred++) difference += (SASJ[irred] - SASJ_N[irred]).norm().item<double>();
     std::cout << "\nBackward propagation vs numerical Jacobian: " << difference << '\n';
+}
+
+int main() {
+    std::cout << "This is a test program on Torch-Chemistry module 'SASintcoord'\n"
+              << "Correct routines should print close to 0\n";
+
+    test_sasic();
+    test_Jacobian();
 }
